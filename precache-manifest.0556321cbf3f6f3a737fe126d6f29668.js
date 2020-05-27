96f7c5556f4d4836cc7acaef7956733a93ce895f
Nico Tambellini <ntambellini13@gmail.com>
2020-05-27 15:53:14 -0600

    Updates

---
+++ precache-manifest.0556321cbf3f6f3a737fe126d6f29668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71db4a371457f7e765e91abde8ac07d",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "6a40608bffd7112a2c4a",
+    "url": "/monsters-rolodex-react/static/css/main.2b9971c5.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex-react/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a40608bffd7112a2c4a",
+    "url": "/monsters-rolodex-react/static/js/main.c86b0094.chunk.js"
+  },
+  {
+    "revision": "4984d3118c6c771f0983",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.42c5dd98.js"
+  }
+]);
